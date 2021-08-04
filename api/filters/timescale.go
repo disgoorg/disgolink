ca4b924b7da5ce116b0ef20b74d04277b1a7aa75
TopiSenpai <tobi@strasserhof.com>
2021-08-04 02:38:03 +0200

    quick fix & go mod tidy

--- api/filters/timescale.go
+++ api/filters/timescale.go
@@ -1,7 +1,7 @@
 package filters
 
 type Timescale struct {
-	Speed float32 `json:"speed,omitempty"`
-	Pitch float32 `json:"pitch,omitempty"`
-	Rate  float32 `json:"rate,omitempty"`
+	Speed float32 `json:"speed"`
+	Pitch float32 `json:"pitch"`
+	Rate  float32 `json:"rate"`
 }
