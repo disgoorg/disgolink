ce747317cf66ccab73cbfe06293773355aafacff
TopiSenpai <tobi@strasserhof.com>
2022-05-01 13:08:39 +0200

    add LocalSourcePlugin for extra data encoding

--- lavalink/lavalink_config.go
+++ lavalink/lavalink_config.go
@@ -12,7 +12,7 @@ func DefaultConfig() *Config {
 	return &Config{
 		Logger:     log.Default(),
 		HTTPClient: &http.Client{Timeout: 10 * time.Second},
-		Plugins:    []any{NewHTTPSourcePlugin()},
+		Plugins:    []any{NewHTTPSourcePlugin(), NewLocalSourcePlugin()},
 	}
 }
 
@@ -62,5 +62,11 @@ func WithUserIDFromBotToken(botToken string) ConfigOpt {
 func WithPlugins(plugins ...any) ConfigOpt {
 	return func(config *Config) {
 		config.Plugins = append(config.Plugins, plugins...)
+	}
+}
+
+func WithOverwritePlugins(plugins ...any) ConfigOpt {
+	return func(config *Config) {
+		config.Plugins = plugins
 	}
 }
