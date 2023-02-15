c07ed5a372060d06fceda6ae3e84502b245a26e4
TopiSenpai <git@topi.wtf>
2023-02-15 04:28:05 +0100

    implement driver.Valuer and sql.Scanner for Track

--- lavalink/track.go
+++ lavalink/track.go
@@ -1,9 +1,35 @@
 package lavalink
 
+import (
+	"database/sql"
+	"database/sql/driver"
+	"errors"
+
+	"github.com/disgoorg/json"
+)
+
+var (
+	_ driver.Valuer = (*Track)(nil)
+	_ sql.Scanner   = (*Track)(nil)
+)
+
 type Track struct {
 	Encoded    string     `json:"encoded"`
 	Info       TrackInfo  `json:"info"`
 	PluginInfo PluginInfo `json:"pluginInfo"`
+}
+
+func (t Track) Value() (driver.Value, error) {
+	return json.Marshal(t)
+}
+
+func (t *Track) Scan(value interface{}) error {
+	b, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+
+	return json.Unmarshal(b, &t)
 }
 
 type TrackInfo struct {
