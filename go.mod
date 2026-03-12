7e43e19fdb22daefc6de7b57fd9d21ea31820ed4
topi314 <git@topi.wtf>
2026-03-12 20:10:35 +0100

    remove testify & update golang to go 1.25.0

--- go.mod
+++ go.mod
@@ -1,17 +1,10 @@
 module github.com/disgoorg/disgolink/v4
 
-go 1.25
+go 1.25.0
 
 require (
 	github.com/disgoorg/json/v2 v2.0.0
 	github.com/disgoorg/omit v1.0.0
 	github.com/disgoorg/snowflake/v2 v2.0.3
 	github.com/gorilla/websocket v1.5.3
-	github.com/stretchr/testify v1.10.0
-)
-
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- lavalink/duration_test.go
+++ lavalink/duration_test.go
@@ -2,51 +2,55 @@ package lavalink
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestDuration_Milliseconds(t *testing.T) {
 	milliseconds := Millisecond * 2
-	assert.Equal(t, int64(2), milliseconds.Milliseconds())
+	equal(t, int64(2), milliseconds.Milliseconds())
 }
 
 func TestDuration_MillisecondsPart(t *testing.T) {
 	seconds := Duration(2345)
-	assert.Equal(t, int64(345), seconds.MillisecondsPart())
+	equal(t, int64(345), seconds.MillisecondsPart())
 }
 
 func TestDuration_Seconds(t *testing.T) {
 	seconds := Second * 2
-	assert.Equal(t, int64(2), seconds.Seconds())
+	equal(t, int64(2), seconds.Seconds())
 }
 
 func TestDuration_SecondsPart(t *testing.T) {
 	seconds := Duration(2345)
-	assert.Equal(t, int64(2), seconds.SecondsPart())
+	equal(t, int64(2), seconds.SecondsPart())
 }
 
 func TestDuration_Minutes(t *testing.T) {
 	minutes := Minute * 2
-	assert.Equal(t, int64(2), minutes.Minutes())
+	equal(t, int64(2), minutes.Minutes())
 }
 
 func TestDuration_MinutesPart(t *testing.T) {
 	minutes := Duration(123456)
-	assert.Equal(t, int64(2), minutes.MinutesPart())
+	equal(t, int64(2), minutes.MinutesPart())
 }
 
 func TestDuration_Hours(t *testing.T) {
 	hours := Hour * 2
-	assert.Equal(t, int64(2), hours.Hours())
+	equal(t, int64(2), hours.Hours())
 }
 
 func TestDuration_HoursPart(t *testing.T) {
 	hours := Duration(7234567)
-	assert.Equal(t, int64(2), hours.HoursPart())
+	equal(t, int64(2), hours.HoursPart())
 }
 
 func TestDuration_Days(t *testing.T) {
 	days := Day * 2
-	assert.Equal(t, int64(2), days.Days())
+	equal(t, int64(2), days.Days())
+}
+
+func equal(t *testing.T, expected int64, actual int64) {
+	if expected != actual {
+		t.Errorf("Expected %d, got %d", expected, actual)
+	}
 }
